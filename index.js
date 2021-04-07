03b381ff7fc8f74bba33ef70e218607fc3d435a1
vahdian <germanpalero@gmail.com>
2021-04-07 12:58:18 +0200

    organised

--- index.js
+++ index.js
@@ -1,58 +1,11 @@
 const express = require("express");
 const app = express();
-const userData = require("./MOCK_DATA.json")
 const { graphqlHTTP } = require("express-graphql");
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList, GraphQLInputObjectType} = graphql;
+const schema = require("./Schemas/index")
 
 //Con userType definimos el modelo de la siguiente manera:
-
-const UserType = new GraphQLObjectType({
-    name:  "User",
-    fields: ()=> ({
-        id: { type: GraphQLInt},
-        first_name: { type: GraphQLString},
-        last_name: { type: GraphQLString},
-        email: { type: GraphQLString},
-        gender: { type: GraphQLString},
-    })
-})
-
-const RootQuery = new GraphQLObjectType({
-    name: "RootQueryType",
-    fields: {
-        getAllUsers: {
-            type: new  GraphQLList(UserType),
-            args: { id: { type: GraphQLInt}},
-            //Con args.id por ejemplo accederemos a la Id
-            resolve(parent, args){
-                return userData
-            }
-        }
-    }
-})
-
-//
-const Mutation = new GraphQLObjectType({
-    name: "Mutation",
-    fields: { 
-        createUser:{
-            type: UserType,
-            args: {
-                first_name: { type: GraphQLString},
-                last_name: { type: GraphQLString},
-                email: { type: GraphQLString},
-                gender: { type: GraphQLString},
-            },
-            resolve(parent, args){
-                userData.push({id: userData.length + 1, first_name: args.first_name, last_name: args.last_name, email: args.email, gender: args.gender})
-                return args
-            }
-        }
-    }
-})
-
-const schema = new GraphQLSchema({query: RootQuery , mutation: Mutation})
 
 app.use("/graphql", graphqlHTTP({
     schema,
